40625bcd2a474f82d85988c47d82f7bcf152d679
evilicebergs <voiced.fair-0a@icloud.com>
2024-08-17 16:16:39 +0300

    Initial Commit

---
+++ JPApexPredators17/JPApexPredators17App.swift
@@ -0,0 +1,17 @@
+//
+//  JPApexPredators17App.swift
+//  JPApexPredators17
+//
+//  Created by Artem Golovchenko on 2024-08-17.
+//
+
+import SwiftUI
+
+@main
+struct JPApexPredators17App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
