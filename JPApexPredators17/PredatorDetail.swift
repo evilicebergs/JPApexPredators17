71737f49520c8edf127210c705e118c72a18b25b
evilicebergs <voiced.fair-0a@icloud.com>
2024-08-25 17:41:42 +0300

    add new serch method, create predatorDetail view, make cells clickable

---
+++ JPApexPredators17/PredatorDetail.swift
@@ -0,0 +1,18 @@
+//
+//  PredatorDetail.swift
+//  JPApexPredators17
+//
+//  Created by Artem Golovchenko on 2024-08-25.
+//
+
+import SwiftUI
+
+struct PredatorDetail: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PredatorDetail()
+}
