1804905c3f2cbed123eef3b8e52f0cb567be7493
evilicebergs <voiced.fair-0a@icloud.com>
2024-09-14 10:59:28 +0300

    add PredatorImage to show full scale image of predators

---
+++ JPApexPredators17/PredatorImage.swift
@@ -0,0 +1,18 @@
+//
+//  PredatorImage.swift
+//  JPApexPredators17
+//
+//  Created by Artem Golovchenko on 2024-09-14.
+//
+
+import SwiftUI
+
+struct PredatorImage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PredatorImage()
+}
